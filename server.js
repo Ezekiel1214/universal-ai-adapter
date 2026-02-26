a415308463dbe409aa5a4f42c0dbdbb75027fc35
iliass bourogui <bouroguis@gmail.com>
2026-02-26 16:34:01 +0400

    fix: build dist and fix server imports

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { UniversalAIAdapter } from './src/index.js';
+import { UniversalAIAdapter } from './dist/index.js';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
